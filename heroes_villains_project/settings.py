66df98e4a949c5a21420182bbfd1512bb16e113a
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2022-12-27 23:52:07 -0500

    fixed issue with secret key exposed

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -1,4 +1,3 @@
-from local_settings import *
 
 """
 Django settings for heroes_villains_project project.
@@ -112,3 +111,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
