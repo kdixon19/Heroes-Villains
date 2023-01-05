f30ba9caf0b05d4ac6af3453ce3801c1c62ceac8
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2023-01-04 21:53:02 -0500

    Fixed issue with null from post request + added a method to return a custom dictionary response if no parameter is set to

--- supers/views.py
+++ supers/views.py
@@ -3,19 +3,30 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Super
+from super_types.models import SuperType
 from .serializers import SuperSerializer
 
 @api_view(['GET','POST'])
 def supers_list(request):
-
     if request.method == 'GET':
-        super_type_name = request.query_params.get('super_types')
-        print(super_type_name)
-        queryset = Super.objects.all()
-        if super_type_name:
-            queryset = queryset.filter(super_types__name=super_type_name)
-        serializer = SuperSerializer(queryset, many=True)
-        return Response(serializer.data)
+        super_type_param = request.query_params.get('super_type')
+        print(super_type_param)
+        supers = Super.objects.all()
+        super_types = SuperType.objects.all()
+        if super_type_param:
+            supers = supers.filter(super_type__type=super_type_param)
+            serializer = SuperSerializer(supers, many=True)
+            return Response(serializer.data)
+        else:
+            custom_response_dictionary = {}
+            for super_type in super_types:
+                supers = Super.objects.filter(super_type_id = super_type.id)
+                super_serializer = SuperSerializer(supers, many=True)
+                custom_response_dictionary[super_type.type] = {
+                    "type": super_type.type,
+                    "name": super_serializer.data
+                }
+            return Response(custom_response_dictionary)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -24,16 +35,16 @@ def supers_list(request):
 
 @api_view(['GET','PUT', 'DELETE'])
 def supers_detail(request,pk):
-    car = get_object_or_404(Super, pk=pk)
+    super = get_object_or_404(Super, pk=pk)
     if request.method == 'GET':   
-        serializer = SuperSerializer(car);
+        serializer = SuperSerializer(super);
         return Response(serializer.data)
     elif request.method == 'PUT':
-        serializer = SuperSerializer(car, data=request.data)
+        serializer = SuperSerializer(super, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
     elif request.method == 'DELETE':
-        car.delete()
+        super.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
 
